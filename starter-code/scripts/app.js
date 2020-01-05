311a563bf1030aff1af1757ec34888a94226990c
Nikita Lukjanov <nlukjanov@gmail.com>
2020-01-05 18:42:23 +0000

    added gameboard and show shape

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -1,59 +1,37 @@
-function init() {
-  //  our code goes here
-  const grid = document.querySelector('.grid')
+// const game = {}
+const squares = []
+let shapeIndex
 
+
+function createGameBoard() {
+  const grid = document.querySelector('.grid')
   const width = 10
   const height = 20
-  const squares = []
-  let playerIndex = 0
 
   Array(height * width)
     .join('.')
     .split('.')
-    .forEach(() => {
-      const square = document.createElement('div')
+    .forEach((square, index) => {
+      square = document.createElement('div')
+      square.id = index
       square.classList.add('grid-item')
       squares.push(square)
       grid.appendChild(square)
     })
-
-  squares[playerIndex].classList.add('player')
-  function handleKeyDown(e) {
-    switch (e.keyCode) {
-      //left
-      case 37:
-        if (playerIndex % width > 0) {
-          playerIndex--
-        }
-        break
-        //up
-      case 38:
-        if (playerIndex - width >= 0) {
-          playerIndex -= width
-        }
-        break
-      //right
-      case 39:
-        if (playerIndex % width < width - 1) {
-          playerIndex++
-        }
-        break
-      //down
-      case 40:
-        if (playerIndex + width < height * width) {
-          playerIndex += width
-        }
-        break
-      default:
-    }
-    console.log(playerIndex)
-    squares.forEach(square => square.classList.remove('player'))
-    squares[playerIndex].classList.add('player')
-  }
-
-  // places player at the starting position when grid has finished building
-
-  window.addEventListener('keydown', handleKeyDown)
 }
 
-window.addEventListener('DOMContentLoaded', init)
+function showShape(startPoint) {
+  shapeIndex = startPoint
+  const cell0 = document.querySelector(`#${CSS.escape(startPoint)}`)
+  const cell1 = document.querySelector(`#${CSS.escape(startPoint + 1)}`)
+  const cell2 = document.querySelector(`#${CSS.escape(startPoint + 2)}`)
+  const cell3 = document.querySelector(`#${CSS.escape(startPoint + 3)}`)
+
+  cell0.classList.add('black')
+  cell1.classList.add('black')
+  cell2.classList.add('black')
+  cell3.classList.add('black')
+}
+
+
+window.addEventListener('DOMContentLoaded', createGameBoard)
