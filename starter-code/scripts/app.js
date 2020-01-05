5636acc64c2ca5beded73ac1c7320d75ec7f4848
Nikita Lukjanov <nlukjanov@gmail.com>
2020-01-05 21:13:02 +0000

    add other shapes

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -1,9 +1,13 @@
 // const game = {}
 const squares = []
 let shapeIndex
+let tetromino
 const width = 10
 const height = 20
-
+let cell0
+let cell1
+let cell2
+let cell3
 
 function createGameBoard() {
   const grid = document.querySelector('.grid')
@@ -19,41 +23,104 @@ function createGameBoard() {
       grid.appendChild(square)
     })
 
-  showShape(65)
+  showShape(65, 'zShape')
 }
 
-function showShape(startPoint) {
-  shapeIndex = startPoint
-  const cell0 = document.querySelector(`#${CSS.escape(startPoint)}`)
-  const cell1 = document.querySelector(`#${CSS.escape(startPoint + 1)}`)
-  const cell2 = document.querySelector(`#${CSS.escape(startPoint + 2)}`)
-  const cell3 = document.querySelector(`#${CSS.escape(startPoint + 3)}`)
+function setShape() {
+  tetromino = 'zShape'
+}
 
+function colorShape() {
   cell0.classList.add('black')
   cell1.classList.add('black')
   cell2.classList.add('black')
   cell3.classList.add('black')
 }
 
+function showShape(startPoint, tetromino) {
+  shapeIndex = startPoint
+  if (tetromino === 'stick') {
+    cell0 = document.querySelector(`#${CSS.escape(startPoint)}`)
+    cell1 = document.querySelector(`#${CSS.escape(startPoint + 1)}`)
+    cell2 = document.querySelector(`#${CSS.escape(startPoint + 2)}`)
+    cell3 = document.querySelector(`#${CSS.escape(startPoint + 3)}`)
+    colorShape()
+    setShape()
+  } else if (tetromino === 'lShape') {
+    cell0 = document.querySelector(`#${CSS.escape(startPoint)}`)
+    cell1 = document.querySelector(`#${CSS.escape(startPoint + 1)}`)
+    cell2 = document.querySelector(`#${CSS.escape(startPoint + 2)}`)
+    cell3 = document.querySelector(`#${CSS.escape(startPoint + 12)}`)
+
+    colorShape()
+    setShape()
+  } else if (tetromino === 'sShape') {
+    cell0 = document.querySelector(`#${CSS.escape(startPoint)}`)
+    cell1 = document.querySelector(`#${CSS.escape(startPoint + 1)}`)
+    cell2 = document.querySelector(`#${CSS.escape(startPoint + 9)}`)
+    cell3 = document.querySelector(`#${CSS.escape(startPoint + 10)}`)
+
+    colorShape()
+    setShape()
+  } else if (tetromino === 'cube') {
+    cell0 = document.querySelector(`#${CSS.escape(startPoint)}`)
+    cell1 = document.querySelector(`#${CSS.escape(startPoint + 1)}`)
+    cell2 = document.querySelector(`#${CSS.escape(startPoint + 10)}`)
+    cell3 = document.querySelector(`#${CSS.escape(startPoint + 11)}`)
+
+    colorShape()
+    setShape()
+  } else if (tetromino === 'tShape') {
+    cell0 = document.querySelector(`#${CSS.escape(startPoint)}`)
+    cell1 = document.querySelector(`#${CSS.escape(startPoint + 9)}`)
+    cell2 = document.querySelector(`#${CSS.escape(startPoint + 10)}`)
+    cell3 = document.querySelector(`#${CSS.escape(startPoint + 11)}`)
+
+    colorShape()
+    setShape()
+  } else if (tetromino === 'jShape') {
+    cell0 = document.querySelector(`#${CSS.escape(startPoint)}`)
+    cell1 = document.querySelector(`#${CSS.escape(startPoint + 1)}`)
+    cell2 = document.querySelector(`#${CSS.escape(startPoint + 2)}`)
+    cell3 = document.querySelector(`#${CSS.escape(startPoint + 10)}`)
+
+    colorShape()
+    setShape()
+  } else if (tetromino === 'zShape') {
+    cell0 = document.querySelector(`#${CSS.escape(startPoint)}`)
+    cell1 = document.querySelector(`#${CSS.escape(startPoint + 1)}`)
+    cell2 = document.querySelector(`#${CSS.escape(startPoint + 11)}`)
+    cell3 = document.querySelector(`#${CSS.escape(startPoint + 12)}`)
+
+    colorShape()
+    setShape()
+  }
+}
+
 function moveLeft() {
+  console.log('tetromino', tetromino)
   if (shapeIndex % width > 0) {
     squares.forEach(square => square.classList.remove('black'))
-    showShape(shapeIndex - 1)
+    showShape(shapeIndex - 1, tetromino)
   }
 }
 
 function moveRight() {
   if (shapeIndex % width < width - 4 /* this number 4 depends on the shape*/) {
     squares.forEach(square => square.classList.remove('black'))
-    showShape(shapeIndex + 1)
+    showShape(shapeIndex + 1, tetromino)
   }
 }
 
 function moveDown() {
   if (shapeIndex + width < height * width) {
     squares.forEach(square => square.classList.remove('black'))
-    showShape(shapeIndex + 10)
+    showShape(shapeIndex + 10, tetromino)
   }
+}
+
+function rotate() {
+  
 }
 
 function handleKeyDown(e) {
@@ -64,9 +131,7 @@ function handleKeyDown(e) {
       break
     //up
     case 38:
-      // if (playerIndex - width >= 0) {
-      //   playerIndex -= width
-      // }
+      rotate()
       break
     //right
     case 39:
